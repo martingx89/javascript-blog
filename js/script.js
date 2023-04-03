c293f30773746973358979b2b0356aae816f7590
Marcin Adamiak <marcin_89@outlook.com>
2023-04-03 18:19:30 +0200

    Extend removal function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 function titleClickHandler(event) {
 	console.log("Link was clicked!");
 	console.log(event);
+
 	/* remove class 'active' from all article links  */
 
 	const activeLinks = document.querySelectorAll(".titles a.active");
@@ -14,6 +15,12 @@ function titleClickHandler(event) {
 	/* add class 'active' to the clicked link */
 
 	/* remove class 'active' from all articles */
+
+	const activeArticles = document.querySelectorAll(".post");
+
+	for (let activeArticle of activeArticles) {
+		activeArticle.classList.remove("active");
+	}
 
 	/* get 'href' attribute from the clicked link */
 
