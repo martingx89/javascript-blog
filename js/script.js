db057d75f60d1b16051b5af8a384a26fd3207c98
Marcin Adamiak <marcin_89@outlook.com>
2023-04-03 17:43:54 +0200

    Add function to remove active class

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ function titleClickHandler(event) {
 	console.log(event);
 	/* remove class 'active' from all article links  */
 
+	const activeLinks = document.querySelectorAll(".titles a.active");
+
+	for (let activeLink of activeLinks) {
+		activeLink.classList.remove("active");
+	}
+
 	/* add class 'active' to the clicked link */
 
 	/* remove class 'active' from all articles */
