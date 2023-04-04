1e20201c430679777eb276ea05e596971c2be642
Marcin Adamiak <marcin_89@outlook.com>
2023-04-04 23:10:54 +0200

    Add generate articles with own titles

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@
 function titleClickHandler(event) {
 	event.preventDefault();
 	const clickedElement = this;
-	// console.log("Link was clicked!");
-	// console.log(event);
 
 	/* remove class 'active' from all article links  */
 
@@ -42,8 +40,44 @@ function titleClickHandler(event) {
 	targetArticle.classList.add("active");
 }
 
-const links = document.querySelectorAll(".titles a");
+const optArticleSelector = ".post",
+	optTitleSelector = ".post-title",
+	optTitleListSelector = ".titles";
 
-for (let link of links) {
-	link.addEventListener("click", titleClickHandler);
+function generateTitleLinks() {
+	/* remove contents of titleList */
+	const titleList = document.querySelector(optTitleListSelector);
+	titleList.innerHTML = "";
+
+	/* for each article */
+	const articles = document.querySelectorAll(optArticleSelector);
+
+	let html = "";
+
+	for (let article of articles) {
+		/* get the article id */
+		const articleId = article.getAttribute("id");
+
+		/* find the title element */
+		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+		// console.log(`ID: ${articleId} - Title: ${articleTitle}`);
+
+		/* create HTML of the link */
+		const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + "</span></a></li>";
+		// console.log(linkHTML);
+
+		/* insert link into titleList */
+		html += linkHTML;
+		console.log(html);
+	}
+	titleList.innerHTML = html;
+
+	const links = document.querySelectorAll(".titles a");
+	console.log(links);
+
+	for (let link of links) {
+		link.addEventListener("click", titleClickHandler);
+	}
 }
+
+generateTitleLinks();
