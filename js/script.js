82bf431015dc9072c9b82a6361f98f58812fa647
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-04-04 06:47:17 +0000

    Add "active" class to target article

--- js/script.js
+++ js/script.js
@@ -29,9 +29,17 @@ function titleClickHandler(event) {
 
 	/* get 'href' attribute from the clicked link */
 
+	const articleSelector = clickedElement.getAttribute("href");
+	console.log("href: ", articleSelector);
+
 	/* find the correct article using the selector (value of 'href' attribute) */
 
+	const targetArticle = document.querySelector(articleSelector);
+	console.log(targetArticle);
+
 	/* add class 'active' to the correct article */
+
+	targetArticle.classList.add("active");
 }
 
 const links = document.querySelectorAll(".titles a");
