9264bccab4f0c3f0e9d897e644ca742cfef95a70
Marcin Adamiak <marcin_89@outlook.com>
2023-04-06 20:38:38 +0200

    Fix singlequotes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function titleClickHandler(event) {
   event.preventDefault();
@@ -6,29 +6,29 @@ function titleClickHandler(event) {
 
   /* remove class 'active' from all article links  */
 
-  const activeLinks = document.querySelectorAll(".titles a.active");
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
-    activeLink.classList.remove("active");
+    activeLink.classList.remove('active');
   }
 
   /* add class 'active' to the clicked link */
 
-  console.log("clickedElement:", clickedElement);
-  clickedElement.classList.add("active");
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll(".post");
+  const activeArticles = document.querySelectorAll('.post');
 
   for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove("active");
+    activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
 
-  const articleSelector = clickedElement.getAttribute("href");
-  console.log("href: ", articleSelector);
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('href: ', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
@@ -37,26 +37,26 @@ function titleClickHandler(event) {
 
   /* add class 'active' to the correct article */
 
-  targetArticle.classList.add("active");
+  targetArticle.classList.add('active');
 }
 
-const optArticleSelector = ".post",
-  optTitleSelector = ".post-title",
-  optTitleListSelector = ".titles";
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
 function generateTitleLinks() {
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = "";
+  titleList.innerHTML = '';
 
   /* for each article */
   const articles = document.querySelectorAll(optArticleSelector);
 
-  let html = "";
+  let html = '';
 
   for (let article of articles) {
     /* get the article id */
-    const articleId = article.getAttribute("id");
+    const articleId = article.getAttribute('id');
 
     /* find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
@@ -72,11 +72,11 @@ function generateTitleLinks() {
   }
   titleList.innerHTML = html;
 
-  const links = document.querySelectorAll(".titles a");
+  const links = document.querySelectorAll('.titles a');
   console.log(links);
 
   for (let link of links) {
-    link.addEventListener("click", titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
   }
 }
 
