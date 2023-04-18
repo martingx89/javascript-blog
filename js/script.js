b9c699d0222e0ef89a04f82ad2c4235256c349be
Marcin Adamiak <marcin_89@outlook.com>
2023-04-18 18:52:53 +0200

    Fix tag template function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const templates = {
   articleLink: Handlebars.compile(
     document.querySelector('#template-article-link').innerHTML
   ),
+  tagLink: Handlebars.compile(
+    document.querySelector('#template-tag-link').innerHTML
+  ),
 };
 const opts = {
   tagSizes: {
@@ -142,7 +145,10 @@ function generateTags() {
 
     for (let tag of articleTagsArray) {
       /* generate HTML of the link */
-      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      // const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+
+      const linkHTMLData = { id: tag, title: tag };
+      const linkHTML = templates.tagLink(linkHTMLData);
 
       /* add generated code to html variable */
       html += linkHTML + ' ';
